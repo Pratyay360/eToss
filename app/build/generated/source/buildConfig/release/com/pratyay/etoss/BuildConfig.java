c3fdfd3e110e3826196f720ea3d1547edeb4ab46
Pratyay360 <pratyaymustafi@gmail.com>
2021-06-10 01:08:11 +0530

    Message

---
+++ app/build/generated/source/buildConfig/release/com/pratyay/etoss/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.pratyay.etoss;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.pratyay.etoss";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
